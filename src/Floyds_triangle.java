d1f78e73d2090b66174e6f7f74e20ebf8b27ba27
AnishaDevi <anishasdevi444@gmail.com>
2024-06-08 23:45:37 +0530

    Floyds Triangle code added

--- src/Floyds_triangle.java
+++ src/Floyds_triangle.java
@@ -9,9 +9,9 @@ public class Floyds_triangle {
         System.out.println("Enter the value of n:");
         int n = sc.nextInt();
 
-        for(int i=0 ; i<=n ; i++)
+        for(int i=1 ; i<=n ; i++)
         {
-            for(int j=0 ; j<=i ; j++)
+            for(int j=1 ; j<=i ; j++)
             {
                 System.out.print(count+" ");
                 count++;
